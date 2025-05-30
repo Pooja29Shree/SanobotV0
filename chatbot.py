1e38747fd721aa91fb95b2ce29d0898f26d79b2a
Pooja29Shree <147960221+Pooja29Shree@users.noreply.github.com>
2025-05-30 10:37:15 +0530

    Replace content with new project

--- chatbot.py
+++ chatbot.py
@@ -2,12 +2,11 @@ import streamlit as st
 from groq_api import call_groq_chat, SYSTEM_PROMPT
 
 st.set_page_config(page_title=" Healthcare Chatbot", layout="wide")
-st.title("AI-Powered Medical Assistant ")
+st.title("Sanobot - Your AI Powered Medical Assistant")
 
 # Initialize session state
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = [SYSTEM_PROMPT]
-
 user_input = st.text_input(" Describe your symptoms or ask a health question:")
 
 if user_input:
